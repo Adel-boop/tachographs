9bc24b6cc4d97ace47f0aacd4b2cd4b834ef2bbc
Adel-boop <adeltskr@mail.ru>
2022-08-09 11:16:58 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -1,22 +1,6 @@
 
 if(window.location == 'https://tacho.sotrans.ru/privacy') {
-    let body = document.querySelector('.politics__privacy-text')
-    let textAnimation = document.querySelector('.politics__privacy-text-animation')
-    body.onscroll = function (e) {
-        if (window.matchMedia('(max-width: 520px)').matches){
-            if(body.scrollTop > body.scrollHeight-400) {
-                textAnimation.classList.remove('politics__privacy-text')
-            } else if (body.scrollTop < body.scrollHeight-400) {
-                textAnimation.classList.add('politics__privacy-text')
-            }
-        } else {
-            if(body.scrollTop > body.scrollHeight-700) {
-                textAnimation.classList.remove('politics__privacy-text')
-            } else if (body.scrollTop < body.scrollHeight-700) {
-                textAnimation.classList.add('politics__privacy-text')
-            }
-        }
-    };
+
 } else {
     let openForm = document.querySelectorAll('.call')
     let closeSucc = document.querySelector('.form__close')
